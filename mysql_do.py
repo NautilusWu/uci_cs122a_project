b516492657368773c0fb360b489e3f3bca0c554e
Yunze Wu <yunzew3@uci.edu>
2025-03-16 15:28:55 -0700

    boring test

--- mysql_do.py
+++ mysql_do.py
@@ -834,7 +834,8 @@ def videos_viewed(rid):
     if not is_exist("videos", "rid", rid):
         print(f"rid {rid} is not exist in table videos.")
         return -1, None
-    query = f"SELECT tb1.rid, tb1.ep_num, tb1.title, tb1.length, tb2.count_uid "
+    # query = f"SELECT tb1.rid, tb1.ep_num, tb1.title, tb1.length, tb2.count_uid "
+    query = f"SELECT tb1.rid, tb1.ep_num, tb1.title, tb1.length, COALESCE(tb2.count_uid, 0) AS count_uid "
     query += f"FROM videos as tb1 "
     query += f"LEFT JOIN "
     query += f"(SELECT rid, COALESCE(ep_num, rid) AS new_ep_num, count(distinct uid) as count_uid "
