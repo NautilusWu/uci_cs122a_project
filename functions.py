c3bb0fb3a20df8b41981a70d898fe8e1f1212803
Yunze Wu <yunzew3@uci.edu>
2025-03-17 02:02:15 -0700

    boring test

--- functions.py
+++ functions.py
@@ -426,11 +426,8 @@ def f_videosviewed(params):
     if rtn >=0 :
         # print(f"RID{' '*2}ep_num{' '*2}title{' '*20}length{' '*2}COUNT")
         for rec in result:
-            count = 0 if rec[4] is None else  int(rec[4])
+            # count = 0 if rec[4] is None else  int(rec[4])
             ep_num = " " if rec[1] is None else  int(rec[1])
-            print(f"{int(rec[0])},{ep_num},{rec[2]},{rec[3]},{count}")
-
-        
-
+            print(f"{int(rec[0])},{ep_num},{rec[2]},{rec[3]},{rec[4]}")
     else:
         return False
